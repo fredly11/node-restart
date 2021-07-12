93aabb4c43bf2d10b905f1b4ab4d297ef79d0b04
unknown <buechelewd@gmail.com>
2021-07-12 16:31:33 -0600

    version 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let port = 3000
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World blargyblarg\n");
+  response.end("Hello World blargyblarg version 2!\n");
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
@@ -13,3 +13,4 @@ server.listen({port});
 
 // Put a friendly message on the terminal
 console.log(`Server running at http://127.0.0.1:${port}/`);
+
